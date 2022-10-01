b13932d684750e19638345c89098811fcba4bbc9
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-01 18:21:51 -0300

    fix: imports

--- src/shared/database/index.ts
+++
@@ -1,1 +0,0 @@
-import { createConnections } from 'typeorm';
--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -1,7 +1,5 @@
 import 'dotenv/config';
 
-import '@shared/database';
-
 import { app } from './app';
 
 const port = process.env.PORT || 3333;
