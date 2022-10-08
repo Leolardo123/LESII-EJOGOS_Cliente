2c9e70918ad10abb8557194cc7a111803a77c04e
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-08 17:03:16 -0300

    partial: custom interface for productDAO

--- src/modules/repositories/DAOProducts.ts
+++ src/modules/repositories/DAOProducts.ts
@@ -1,7 +1,7 @@
 import Product from "@modules/models/products/Product";
 import { DAOAbstract } from "./abstract/DAOAbstract";
 
-export class DAOProduct extends DAOAbstract<Product> {
+export class DAOProduct extends DAOAbstract<Product> implements DAOProduct {
   constructor() {
     super(Product);
   }
