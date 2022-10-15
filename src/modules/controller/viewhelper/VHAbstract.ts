7865b4c5451cdf86a9652c7bd0f4c173d3ef5b25
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-15 11:48:43 -0300

    fix: abstract find entity

--- src/modules/controller/viewhelper/VHAbstract.ts
+++ src/modules/controller/viewhelper/VHAbstract.ts
@@ -5,9 +5,10 @@ import { IGetEntity } from "./interface/IViewHelper";
 export abstract class VHAbstract {
     abstract getEntity(req: Request): Domain;
     findEntity(req: Request): IGetEntity {
+        const entity = this.getEntity(req);
         return {
-            entity: this.getEntity(req),
-            relations: []
+            entity,
+            whereParams: { where: entity }
         }
     }
     setView(req: Request, res: any, result: Domain[] | Domain | string): void {
