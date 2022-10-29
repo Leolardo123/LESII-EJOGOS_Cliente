c4903170ab279bef51bfa3975a3db75ebdee1ec2
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-10-29 16:32:22 -0300

    fix: missing uploadConfig destination

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -23,6 +23,7 @@ export const uploadConfig = {
   uploadsFolder: path.resolve(tmpFolder),
   multer: {
     storage: multer.diskStorage({
+      destination: tmpFolder,
       filename: (request, file, callback) => {
         const fileHash = crypto.randomBytes(10).toString('hex');
         const formattedOriginalName = file.originalname
