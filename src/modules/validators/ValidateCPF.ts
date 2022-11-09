67dc40e5e0366b007583423dd26f5974ae7b9f8d
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-11-08 22:55:45 -0300

    fix: simplify validate CPF

--- src/modules/validators/ValidateCPF.ts
+++ src/modules/validators/ValidateCPF.ts
@@ -9,7 +9,8 @@ export class ValidateCPF implements IValidate {
             throw new Error('CPF é obrigatório (CPF).');
         }
 
-        if (!/(\d{3}\.){2}\d{3}\-\d{2}/.test(entity.cpf)) {
+        //(000.000.000-00)
+        if (!/\d{3}\.\d{3}\.\d{3}\-\d{2}/.test(entity.cpf)) {
             throw new Error(`O CPF ${entity.cpf} não é válido.`);
         }
     }
