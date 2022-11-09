0846aa5b8113ab52f7f2d6cbe6d8d46988bfe86c
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-11-08 22:54:19 -0300

    fix: validate CPF

--- src/modules/validators/ValidateCPF.ts
+++ src/modules/validators/ValidateCPF.ts
@@ -1,15 +1,16 @@
 import Person from "../models/users/Person";
 import { IValidate } from "./IValidate";
 
-export class ValidateCPF implements IValidate{
+export class ValidateCPF implements IValidate {
     constructor(
-    ){}
+    ) { }
     async validate(entity: Person): Promise<void> {
-        if(!entity.cpf){
+        if (!entity.cpf) {
             throw new Error('CPF é obrigatório (CPF).');
         }
-        if(!/\d{11}/.test(entity.cpf)){
-            throw new Error(`O CPF ${entity.cpf} deve conter 11 dígitos.`);
+
+        if (!/(\d{3}\.){2}\d{3}\-\d{2}/.test(entity.cpf)) {
+            throw new Error(`O CPF ${entity.cpf} não é válido.`);
         }
     }
 }
