436505ffc1bf9ee74ac83c414bb6aa8377b0640c
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-11-26 15:38:41 -0300

    fix: validate empty new cart

--- src/modules/validators/ValidateCartItem.ts
+++ src/modules/validators/ValidateCartItem.ts
@@ -3,28 +3,28 @@ import { DAOProduct } from "@modules/repositories/DAOProducts";
 import CartItem from "../models/sales/CartItem";
 import { IValidate } from "./IValidate";
 
-export class ValidateCartItem implements IValidate{
-    constructor(){}
+export class ValidateCartItem implements IValidate {
+    constructor() { }
     async validate(entity: CartItem): Promise<void> {
-        if(!(entity instanceof CartItem)){
+        if (!(entity instanceof CartItem)) {
             throw new Error('Entidade inválida, esperava item do carrinho.');
         }
 
-        const where = entity.cart.id ? { 
+        const where = entity.cart.id ? {
             id: entity.cart.id
-        } : { 
+        } : {
             person_id: entity.cart.person.id,
             isOpen: true
         };
-        
+
         const daoCart = new DAOCart();
-        const cartExists = await daoCart.findOne({ 
+        const cartExists = await daoCart.findOne({
             where,
             relations: ['items', 'person']
         })
 
-        if(cartExists){
-            if(!cartExists.isOpen || cartExists.purchase){
+        if (cartExists) {
+            if (!cartExists.isOpen || cartExists.purchase) {
                 throw new Error('Carrinho já foi finalizado.');
             }
 
@@ -32,26 +32,26 @@ export class ValidateCartItem implements IValidate{
                 item => item.product.id === entity.product.id
             );
 
-            if(itemExists){
+            if (itemExists) {
                 entity.id = itemExists.id;
 
-                if(!entity.quantity){
+                if (!entity.quantity) {
                     entity.quantity = itemExists.quantity;
                 }
 
-                if(entity.product.id != itemExists.product.id){
+                if (entity.product.id != itemExists.product.id) {
                     throw new Error('Produto inválido.');
                 }
             }
 
             entity.cart.id = cartExists.id;
         } else {
-            if(!entity.cart.id){
+            if (entity.cart.id) {
                 throw new Error('Carrinho não encontrado.');
             }
         }
 
-        if(!entity.product || !entity.product.id){
+        if (!entity.product || !entity.product.id) {
             throw new Error('Produto não especificado.');
         }
 
@@ -60,20 +60,20 @@ export class ValidateCartItem implements IValidate{
             where: { id: entity.product.id }
         });
 
-        if(!productExists || !productExists.isActive){
+        if (!productExists || !productExists.isActive) {
             throw new Error('Produto não existe ou está indísponível no momento.');
         }
 
-        if(!entity.quantity){
+        if (!entity.quantity) {
             entity.quantity = 1;
         }
 
         const reservedQuantity = await daoProduct.getReservedQuantity(
-            productExists, 
+            productExists,
             [entity.cart]
         );
 
-        if(entity.quantity > (productExists.stock - reservedQuantity)){
+        if (entity.quantity > (productExists.stock - reservedQuantity)) {
             throw new Error('Quantidade indisponível em estoque.');
         }
 
